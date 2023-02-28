639e58eeb60e5a8f83eb26d0fb22c76e2e113b7b
Francisco Javier Lendínez Tirado <fjlendinez@gmail.com>
2023-02-28 20:27:37 +0100

    Base (based of fuzzy)

---
+++ project_name/docker/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+tailwindcss -i {{ project_name }}/static/css/style.css -o {{ project_name }}/static/css/output.css --minify
+python manage.py collectstatic --noinput
+python manage.py migrate
+python manage.py makesuperuser
+gunicorn config.wsgi --config="docker/gunicorn.conf.py"
