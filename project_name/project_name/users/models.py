617f77384a151f184660e85fc928721ff0d297f3
Francisco Javier Lendínez Tirado <fjlendinez@gmail.com>
2023-03-01 11:25:32 +0100

    Update models.py

--- project_name/project_name/users/models.py
+++ project_name/project_name/users/models.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    username = None # To delete the field username in abstract model
+    username = None  # To delete the field username in abstract model
+    email = models.CharField(
+        _("email"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        error_messages={
+            "unique": _("A user with that email already exists."),
+        },
+    )
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["email"]
+    REQUIRED_FIELDS = []
