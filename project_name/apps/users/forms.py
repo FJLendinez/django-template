4f13a37e58a6e3e278d805112a176fd650e6a660
Francisco Javier Lendínez Tirado <fjlendinez@gmail.com>
2023-03-16 20:06:04 +0100

    Delete project_name directory

--- project_name/apps/users/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
-from django.http import HttpRequest
-
-
-class UserSignupForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ("email", "password1", "password2")
--- project_name/docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tailwindcss -i {{ project_name }}/static/css/style.css -o {{ project_name }}/static/css/output.css --minify
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py makesuperuser
-gunicorn config.wsgi --config="docker/gunicorn.conf.py"
