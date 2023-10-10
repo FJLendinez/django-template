a119ce1dae15718194c2c5149eec9c235a05e1d3
Francisco Javier Lendínez Tirado <fjlendinez@gmail.com>
2023-10-10 13:09:51 +0200

    Update tailwind.config.js

--- project_name/tailwind.config.js
+++ project_name/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['templates/*.html', "templates/**/*.html"],
+  content: ['templates/**/*.html', "apps/*/templates/**/*.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 }
